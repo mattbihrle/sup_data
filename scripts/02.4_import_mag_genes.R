aea2b4c6b559a24ad93c3cfdd01d366a19975089
Matt Bihrle <matt.bihrle@gmail.com>
2026-07-17 17:01:51 -0500

    7/17/26

--- scripts/02.4_import_mag_genes.R
+++ scripts/02.4_import_mag_genes.R
@@ -8,60 +8,56 @@ load("output/data/mt_mag.RData")
 bin_abund <- mt_mag$otu_table
 sample_meta <- mt_mag$sample_table
 
-# Create foam table where genes are rows and bins are columns. Values represent precense/absence.
-foam_rollup_df <- read_tsv("output/data/metabolism/foam_rollup_all.tsv")
+# Create kegg table where genes are rows and bins are columns. Values represent presence/absence.
 
-length(unique(foam_rollup_df$bin))
-# files <- list.files("msi_downloads/metacerb/all_files/", full.names = T)
-# files[1]
+# First make a list of all the bins we want to use
+bins <- mt_mag_large$tax_table$bin |>
+  str_remove("b__") |>
+  str_split_fixed("_", n = 3) |>
+  as.data.frame()
 
-# foam_files <- files |>
-#   str_extract(".*FOAM.*") |> 
-#   na.omit()
+bins <- paste0(bins$V1, "_", bins$V2,"_", "MAGScoT_cleanbin_000", bins$V3)
+test <- read_tsv(files[1])
 
-# # Create FOAM df -----------------------------------------------------------
-# for(i in 1:length(foam_files)){
-# bin_name <- str_extract(foam_files[i], "WM.*_.*_.*_.*_[0-9]{6}") |> 
-#   str_remove('MAGScoT_cleanbin_000')
-
-# print(bin_name)
-
-#   int_df <- read_tsv(foam_files[i]) |> 
-#       mutate(bin = bin_name, .before = Id) |> 
-#       rename_with(tolower)
-#   # bin_name <- core_bins[i]
-#   # #Set file path here
-#   # path <- file.path("msi_downloads", "metacerb", paste0("MetaCerb_",bin_name, ".fa"),
-#   #         "step_10-visualizeData",paste0("prodigal_",bin_name), "/")
-#   # path
-#   # Select the file you want with the regex here
-
-#   if(i == 1){
-#     gene_df <- int_df
-#   } else {
-#     gene_df <- bind_rows(gene_df, int_df)
-#   }
-# }
-# foam_df <- gene_df
-# Cut out to retain the bins that are in the mag tax table
-foam_df_cut <- foam_rollup_df |> 
-  slice(which(foam_rollup_df$bin %in% rownames(mt_mag$tax_table)))
+# turn that into a list of file names to import 
+file_start <- "msi_downloads/rollup/kegg/rollup_prodigal_"
+file_end <- "-KOFam_all_KEGG.tsv"
 
 
-foam_table <- foam_df_cut |> 
+files <- paste0(file_start, bins, file_end)
+files[1]
+
+# Create kegg df -----------------------------------------------------------
+for(i in 1:length(files)){
+bin_name <- str_extract(files[i], "WM.*_.*_.*_.*_[0-9]{6}") |>
+  str_remove('MAGScoT_cleanbin_000')
+print(bin_name)
+  int_df <- read_tsv(files[i]) |>
+      mutate(bin = bin_name, .before = everything()) |>
+      rename_with(tolower)
+
+  if(i == 1){
+    gene_df <- int_df
+  } else {
+    gene_df <- bind_rows(gene_df, int_df)
+  }
+}
+kegg_df <- gene_df
+
+kegg_table <- kegg_df |> 
   group_by(bin, id) |> 
   tally(count)
-foam_table |> 
+kegg_table |> 
   ggplot(aes(x = n)) +
   geom_histogram()
 
 # check to make sure that I have the correct bins
 
-length(unique(foam_df_cut$bin))
+length(unique(kegg_df$bin))
 
 # Okay it looks like there are not really many issues where need to worry about there being more than 1 gene copy.
 
-foam_table_wide <- foam_table |> 
+kegg_table_wide <- kegg_table |> 
   pivot_wider(names_from = bin, values_from = n) |> 
   column_to_rownames("id") |> 
   # Make it a presence abesnce thing (change things here if I need to)
@@ -72,7 +68,7 @@ foam_table_wide <- foam_table |>
 # -------------------------------------------------------------------
 # Matrix multiplication: 
 # (Genes x Bins matrix) %*% (Bins x Samples matrix) = (Genes x Samples matrix)
-gene_abund_matrix <- as.matrix(foam_table_wide) %*% as.matrix(bin_abund)
+gene_abund_matrix <- as.matrix(kegg_table_wide) %*% as.matrix(bin_abund)
 
 # Convert back to a dataframe for the microtable
 gene_abund <- as.data.frame(gene_abund_matrix)
@@ -80,9 +76,11 @@ gene_abund <- as.data.frame(gene_abund_matrix)
 
 # Create a tax_table, but only keep one of the l1, l2, l3 explanations. Here we are losing some information about what gene pathways genes are being used for. 
 
-tax_table <- foam_df_cut |> 
+tax_table <- kegg_df |> 
   mutate(func = `function`) |> 
-  select(bin, id, l1, l2, l3, l4, func) |> 
+  mutate(path = str_extract(l3, "ko[0-9]{5}")) |>
+  mutate(l3 = str_remove(l3, "\\s\\[.*\\]")) |> 
+  select(bin, id, l1, l2, l3, path, func) |> 
   distinct() |> 
   group_by(id) |> 
   slice_head() |> 
@@ -94,11 +92,6 @@ tax_table <- foam_df_cut |>
 mt_gene_mag <- microtable$new(otu_table = gene_abund, 
                               sample_table = sample_meta, 
                               tax_table = tax_table)
-
-
-# Now do it again with the microtrait in formation 
-
-load(mt_mag_large)
 
 # Save microtable object
 
--- scripts/scratch.R
+++ scripts/scratch.R
@@ -10,4 +10,55 @@ p2 <- ggplot(data, aes(x = Abundance, y = Growth_Rate, color = Species)) +
 # Print the plots
 print(p1)
 print(p2)
-|> |> |> 
+
+rollup <- read_tsv("msi_downloads/metacerb/all_files/uba_genomes/MetaCerb_WM15_S10_MAGScoT_cleanbin_000035/final/rollup/rollup_prodigal_WM15_S10_MAGScoT_cleanbin_000035-KOFam_all_KEGG.tsv") |> 
+  arrange(ID) |> 
+  select(ID) |> 
+  distinct()
+
+summary <- read_tsv("msi_downloads/metacerb/all_files/uba_genomes/MetaCerb_WM15_S10_MAGScoT_cleanbin_000035/final/annotations-prodigal_WM15_S10_MAGScoT_cleanbin_000035/final_annotation_summary.tsv") |> 
+  drop_na(best_hit) |> 
+  arrange(best_hit) |>
+  select(best_hit) |> 
+  distinct()
+waldo::compare(rollup$ID, summary$best_hit)
+
+kegg <- read_tsv("output/data/kegg_output/WM15_S10_035_kegg.tsv") |> 
+  arrange(id) |> 
+  distinct() |> 
+  mutate(kegg = TRUE)
+
+rollup <- kegg_table |> 
+  filter(bin == "WM15_S10_035") |> 
+  select(-n) |> 
+  distinct() |> 
+  arrange(id) |> 
+  mutate(rollup = TRUE)
+sum((rollup$id %in% kegg$id))
+
+sum(kegg$id %in% rollup$id)
+
+new <- rollup |> 
+  full_join(kegg, by = c("id", "bin"))
+
+
+foam <- read_tsv("msi_downloads/metacerb/all_files/uba_genomes/MetaCerb_WM15_S10_MAGScoT_cleanbin_000035/final/annotations-prodigal_WM15_S10_MAGScoT_cleanbin_000035/annotation_summary_KOFam_all_FOAM.tsv") |> 
+  drop_na(best_hit)
+kegg <- read_tsv("msi_downloads/metacerb/all_files/uba_genomes/MetaCerb_WM15_S10_MAGScoT_cleanbin_000035/final/annotations-prodigal_WM15_S10_MAGScoT_cleanbin_000035/annotation_summary_KOFam_all_KEGG.tsv") |> 
+  drop_na(best_hit)
+sum <- read_tsv("msi_downloads/metacerb/all_files/uba_genomes/MetaCerb_WM15_S10_MAGScoT_cleanbin_000035/final/annotations-prodigal_WM15_S10_MAGScoT_cleanbin_000035/final_annotation_summary.tsv") |> 
+  drop_na(best_hit) 
+
+foam$best_hit |> 
+  unique() |> 
+  length()
+
+kegg$best_hit |> 
+  unique() |> 
+  length()
+
+sum$best_hit |> 
+  unique() |> 
+  length()
+
+# Starting from here, I should see if I can just remove the duplicates. 
